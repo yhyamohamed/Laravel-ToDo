825271cad69981d1ef5c1b7c8921b29b351898b1
yhyamohamed <yhyashehata1@gmail.com>
2022-06-16 19:22:15 +0200

    migrations,modeland controller for todos

---
+++ Todo/database/seeders/TodosSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TodosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
